99b97c5dbe7c294f59cdc9c9c182c67017270c83
alex-gg-hsx21 <alex.golovanik@bk.ru>
2025-04-24 19:01:28 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".burger");
+const menuMobile = document.querySelector(".menu-mobile");
+
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
+Получаем элементы
